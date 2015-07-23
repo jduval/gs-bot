02cd5e61b5eddf397f13cf69126876206b0827bf
Jérôme Duval <jduval@accengage.com>
2015-07-23 17:24:15 +0200

    Use environnement variables instead of JSON config file to deploy on heroku.

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,10 @@
-var fs = require('fs');
 var TelegramBot = require('node-telegram-bot-api');
 
-var config = JSON.parse(fs.readFileSync(__dirname + '/../config.json'));
-
-var bot = new TelegramBot(config.token, {polling: true});
+var bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
   var text = msg.text;
 
   if (/^\/tricount/.test(text))
-    bot.sendMessage(chatId, config.tricount.url);
+    bot.sendMessage(chatId, process.env.TRICOUNT_URL);
 });
