6fb2582e496b3233be38e82cf3c18397f45d899f
Jimmy Oliger <hi@jimmyoliger.com>
2015-07-31 15:42:17 +0200

    use estcequecestbientotleweekend API

--- src/is-it-the-weekend-yet.js
+++ src/is-it-the-weekend-yet.js
@@ -1,15 +1,20 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
-const URL = 'http://estcequecestbientotleweekend.fr';
+const URL = 'http://estcequecestbientotleweekend.fr/api';
 const SELECTOR = '.msg';
 
 function isItTheWeekendYet(callback) {
   request(URL, function (err, response, body) {
     if (!err && response.statusCode == 200) {
-      var d = cheerio.load(body);
+      json = JSON.parse(body);
 
-      callback(null, d(SELECTOR).text().trim());
+      var r = json.text;
+      if (json.subtext) {
+        r += ', ' + json.subtext
+      }
+
+      callback(null, r.trim());
     } else {
       callback(new Error('Not able to fetch ' + URL));
     }
